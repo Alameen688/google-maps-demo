d78c8602436a1e6a30e0a880bb1ba47aaed39e50
Alameen688 <ogundiran12@gmail.com>
2019-11-09 09:40:27 +0100

    Add google map services

---
+++ lib/services/google_maps_service.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' show Client, Response;
+import 'dart:convert';
+
+// const apiKey = 'AIzaSyBhDflq5iJrXIcKpeq0IzLQPQpOboX91lY';
+const apiKey = 'AIzaSyBeWPTHzuEOemzDus-VS3vSMR5JqqD5I-U';
+
+class GoogleMapsService {
+  static const BASE_URL = 'https://maps.googleapis.com/maps/api';
+  String placesSessionToken = '324354342'; //use random string
+  final Client apiClient = Client();
+
+  Future<String> getRouteCoordinates(LatLng l1, LatLng l2) async {
+    String url =
+        '$BASE_URL/directions&origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey';
+    Response response = await apiClient.get(url);
+    Map values = jsonDecode(response.body);
+    return values['routes'][0]['overview_polyline']['points'];
+  }
+
+  Future<List> autocomplete(input) async {
+    String url =
+        '$BASE_URL/place/autocomplete/json?input=$input&sessiontoken=$placesSessionToken&key=$apiKey';
+    List result = [];
+    try {
+      Response response = await apiClient.get(url);
+      Map values = jsonDecode(response.body);
+      // print(values);
+      result = values['predictions'];
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+}
